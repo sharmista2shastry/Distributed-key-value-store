d7e9f7367d9d2577117402126ec1ded5a9708f44
Sharmista Shastry <sharmistashastry@Sharmistas-Air.lan>
2023-10-30 02:05:43 -0400

    completed part B

--- src/kvpaxos/client.go
+++ src/kvpaxos/client.go
@@ -1,77 +1,110 @@
 package kvpaxos
 
-import "net/rpc"
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	mrand "math/rand"
+	"net/rpc"
+	"time"
+)
 
+// nrand generates a random int64 value.
+func nrand() int64 {
+	max := big.NewInt(int64(1) << 62)
+	bigInt, _ := rand.Int(rand.Reader, max)
+	x := bigInt.Int64()
+	return x
+}
+
+// Clerk represents a client that interacts with the KVPaxos servers.
 type Clerk struct {
-  servers []string
-  // You will have to modify this struct.
+	servers  []string
+	deleteOp int64 // Used to remove successful operations from the cache.
 }
 
-
+// MakeClerk creates a new Clerk instance with the given server addresses.
 func MakeClerk(servers []string) *Clerk {
-  ck := new(Clerk)
-  ck.servers = servers
-  // You'll have to add code here.
-  return ck
+	return &Clerk{
+		servers:  servers,
+		deleteOp: 0,
+	}
 }
 
-//
-// call() sends an RPC to the rpcname handler on server srv
-// with arguments args, waits for the reply, and leaves the
-// reply in reply. the reply argument should be a pointer
-// to a reply structure.
-//
-// the return value is true if the server responded, and false
-// if call() was not able to contact the server. in particular,
-// the reply's contents are only valid if call() returned true.
-//
-// you should assume that call() will time out and return an
-// error after a while if it doesn't get a reply from the server.
-//
-// please use call() to send all RPCs, in client.go and server.go.
-// please don't change this function.
-//
-func call(srv string, rpcname string,
-          args interface{}, reply interface{}) bool {
-  c, errx := rpc.Dial("unix", srv)
-  if errx != nil {
-    return false
-  }
-  defer c.Close()
-    
-  err := c.Call(rpcname, args, reply)
-  if err == nil {
-    return true
-  }
+// call sends an RPC to the rpcname handler on server srv with arguments args,
+// waits for the reply, and leaves the reply in reply. The reply argument should
+// be a pointer to a reply structure. The return value is true if the server
+// responded, and false if call was not able to contact the server.
+func call(srv string, rpcName string, args interface{}, reply interface{}) bool {
+	c, err := rpc.Dial("unix", srv)
+	if err != nil {
+		return false
+	}
+	defer c.Close()
 
-  fmt.Println(err)
-  return false
+	err = c.Call(rpcName, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
 }
 
-//
-// fetch the current value for a key.
-// returns "" if the key does not exist.
-// keeps trying forever in the face of all other errors.
-//
+// Get fetches the current value for a key. It returns "" if the key does not exist.
+// It keeps trying forever in the face of all other errors.
 func (ck *Clerk) Get(key string) string {
-  // You will have to modify this function.
-  return ""
+	args := GetArgs{
+		Key:    key,
+		Nrand:  nrand(),
+		Delete: ck.deleteOp,
+	}
+	r := 100
+	to := time.Duration(r) * time.Millisecond
+
+	for {
+		k := mrand.Intn(len(ck.servers))
+		var reply GetReply
+		ok := call(ck.servers[k], "KVPaxos.Get", args, &reply)
+		if ok {
+			ck.deleteOp = args.Nrand
+			return reply.Value
+		}
+		time.Sleep(to)
+	}
 }
 
-//
-// set the value for a key.
-// keeps trying until it succeeds.
-//
-func (ck *Clerk) PutExt(key string, value string, dohash bool) string {
-  // You will have to modify this function.
-  return ""
+// PutExt sets the value for a key. It keeps trying until it succeeds.
+func (ck *Clerk) PutExt(key string, value string, doHash bool) string {
+	args := PutArgs{
+		Key:    key,
+		Value:  value,
+		DoHash: doHash,
+		Nrand:  nrand(),
+		Delete: ck.deleteOp,
+	}
+	r := 100
+	to := time.Duration(r) * time.Millisecond
+
+	for {
+		k := mrand.Intn(len(ck.servers))
+		var reply PutReply
+		ok := call(ck.servers[k], "KVPaxos.Put", args, &reply)
+		if ok {
+			ck.deleteOp = args.Nrand
+			return reply.PreviousValue
+		}
+		time.Sleep(to)
+	}
+	return ""
 }
 
+// Put sets the value for a key.
 func (ck *Clerk) Put(key string, value string) {
-  ck.PutExt(key, value, false)
+	ck.PutExt(key, value, false)
 }
+
+// PutHash sets the value for a key using a hash function.
 func (ck *Clerk) PutHash(key string, value string) string {
-  v := ck.PutExt(key, value, true)
-  return v
+	return ck.PutExt(key, value, true)
 }
--- src/kvpaxos/server.go
+++ src/kvpaxos/server.go
@@ -1,125 +1,237 @@
 package kvpaxos
 
-import "net"
-import "fmt"
-import "net/rpc"
-import "log"
-import "paxos"
-import "sync"
-import "os"
-import "syscall"
-import "encoding/gob"
-import "math/rand"
+import (
+	"encoding/gob"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"net/rpc"
+	"os"
+	"paxos"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+)
 
-const Debug=0
+const Debug = 0
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
-  if Debug > 0 {
-    log.Printf(format, a...)
-  }
-  return
+	if Debug > 0 {
+		log.Printf(format, a...)
+	}
+	return
 }
 
-
 type Op struct {
-  // Your definitions here.
-  // Field names must start with capital letters,
-  // otherwise RPC will break.
+	// Your definitions here.
+	// Field names must start with capital letters,
+	// otherwise RPC will break.
+	Seq           int
+	OperationType string
+	Key           string
+	Value         string
+	OperationId   int64
 }
 
 type KVPaxos struct {
-  mu sync.Mutex
-  l net.Listener
-  me int
-  dead bool // for testing
-  unreliable bool // for testing
-  px *paxos.Paxos
+	mu         sync.Mutex
+	l          net.Listener
+	me         int
+	dead       bool // for testing
+	unreliable bool // for testing
+	px         *paxos.Paxos
 
-  // Your definitions here.
+	// Your definitions here.
+	KVmap       map[string]string
+	updatedTill int              //This is the sequence number till which the kv store is up to date
+	ans         map[int64]string //This stores the answers to pass unreliable
 }
 
+func (kv *KVPaxos) syncOps(details Op) string {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	// Check if the result is already in the cache
+	if val, ok := kv.ans[details.OperationId]; ok {
+		return val
+	}
+
+	// Initialize variables
+	ans := ""
+	to := 10 * time.Millisecond
+
+	// Loop until the operation is completed
+	for {
+		seq := kv.updatedTill
+		decided, value := kv.px.Status(seq)
+		if decided {
+			// Update timeout if necessary
+			if to > 1*time.Second {
+				to = 1 * time.Second
+			}
+
+			// Update state based on operation type
+			v := value.(Op)
+			if v.OperationType == "Put" {
+				kv.KVmap[v.Key] = v.Value
+			} else if v.OperationType == "Put hash" {
+				ans = kv.KVmap[v.Key]
+				kv.KVmap[v.Key] = strconv.Itoa(int(hash(ans + v.Value)))
+			} else if v.OperationType == "Get" {
+				ans = kv.KVmap[v.Key]
+			}
+
+			// Cache the result and check if the operation is done
+			kv.ans[v.OperationId] = ans
+			if details.OperationId == v.OperationId {
+				break
+			}
+
+			// Update sequence number and timeout
+			seq++
+			kv.updatedTill++
+			to = 10 * time.Millisecond
+
+		} else {
+			// Start the operation and wait for it to complete or timeout
+			details.Seq = seq
+			kv.px.Start(seq, details)
+			time.Sleep(to)
+			if to < 20*time.Second {
+				to *= 2
+			}
+		}
+	}
+
+	// Mark the operation as done and cache the result
+	kv.px.Done(kv.updatedTill)
+	kv.updatedTill++
+	kv.ans[details.OperationId] = ans
+
+	return ans
+}
 
 func (kv *KVPaxos) Get(args *GetArgs, reply *GetReply) error {
-  // Your code here.
-  return nil
+	// Create the operation details
+	operationDetails := Op{
+		OperationType: "Get",
+		Key:           args.Key,
+		Value:         "",
+		OperationId:   args.Nrand,
+	}
+
+	// Execute the operation and return the result
+	reply.Value = kv.syncOps(operationDetails)
+
+	// Delete the cached result if requested
+	if args.Delete != 0 {
+		kv.mu.Lock()
+		delete(kv.ans, args.Delete)
+		kv.mu.Unlock()
+	}
+
+	return nil
 }
 
 func (kv *KVPaxos) Put(args *PutArgs, reply *PutReply) error {
-  // Your code here.
+	// Create operation details
+	operationDetails := Op{
+		OperationType: "Put",
+		Key:           args.Key,
+		Value:         args.Value,
+		OperationId:   args.Nrand,
+	}
+	if args.DoHash {
+		operationDetails.OperationType = "Put hash"
+	}
 
-  return nil
+	// Execute operation and update reply
+	reply.PreviousValue = kv.syncOps(operationDetails)
+
+	// Delete operation from cache if necessary
+	if args.Delete != 0 {
+		kv.mu.Lock()
+		if _, ok := kv.ans[args.Delete]; ok {
+			delete(kv.ans, args.Delete)
+		}
+		kv.mu.Unlock()
+	}
+
+	return nil
 }
 
 // tell the server to shut itself down.
 // please do not change this function.
 func (kv *KVPaxos) kill() {
-  DPrintf("Kill(%d): die\n", kv.me)
-  kv.dead = true
-  kv.l.Close()
-  kv.px.Kill()
+	DPrintf("Kill(%d): die\n", kv.me)
+	kv.dead = true
+	kv.l.Close()
+	kv.px.Kill()
 }
 
-//
 // servers[] contains the ports of the set of
 // servers that will cooperate via Paxos to
 // form the fault-tolerant key/value service.
 // me is the index of the current server in servers[].
-// 
 func StartServer(servers []string, me int) *KVPaxos {
-  // call gob.Register on structures you want
-  // Go's RPC library to marshall/unmarshall.
-  gob.Register(Op{})
+	// call gob.Register on structures you want
+	// Go's RPC library to marshall/unmarshall.
+	gob.Register(Op{})
 
-  kv := new(KVPaxos)
-  kv.me = me
+	kv := new(KVPaxos)
+	kv.me = me
 
-  // Your initialization code here.
+	// Your initialization code here.
+	kv.KVmap = make(map[string]string)
+	kv.ans = make(map[int64]string)
+	kv.updatedTill = 0
+	// go kv.fetchOps()
 
-  rpcs := rpc.NewServer()
-  rpcs.Register(kv)
+	rpcs := rpc.NewServer()
+	rpcs.Register(kv)
 
-  kv.px = paxos.Make(servers, me, rpcs)
+	kv.px = paxos.Make(servers, me, rpcs)
 
-  os.Remove(servers[me])
-  l, e := net.Listen("unix", servers[me]);
-  if e != nil {
-    log.Fatal("listen error: ", e);
-  }
-  kv.l = l
+	os.Remove(servers[me])
+	l, e := net.Listen("unix", servers[me])
+	if e != nil {
+		log.Fatal("listen error: ", e)
+	}
+	kv.l = l
 
+	// please do not change any of the following code,
+	// or do anything to subvert it.
 
-  // please do not change any of the following code,
-  // or do anything to subvert it.
+	go func() {
+		for kv.dead == false {
+			conn, err := kv.l.Accept()
+			if err == nil && kv.dead == false {
+				if kv.unreliable && (rand.Int63()%1000) < 100 {
+					// discard the request.
+					conn.Close()
+				} else if kv.unreliable && (rand.Int63()%1000) < 200 {
+					// process the request but force discard of reply.
+					c1 := conn.(*net.UnixConn)
+					f, _ := c1.File()
+					err := syscall.Shutdown(int(f.Fd()), syscall.SHUT_WR)
+					if err != nil {
+						fmt.Printf("shutdown: %v\n", err)
+					}
+					go rpcs.ServeConn(conn)
+				} else {
+					go rpcs.ServeConn(conn)
+				}
+			} else if err == nil {
+				conn.Close()
+			}
+			if err != nil && kv.dead == false {
+				fmt.Printf("KVPaxos(%v) accept: %v\n", me, err.Error())
+				kv.kill()
+			}
+		}
+	}()
 
-  go func() {
-    for kv.dead == false {
-      conn, err := kv.l.Accept()
-      if err == nil && kv.dead == false {
-        if kv.unreliable && (rand.Int63() % 1000) < 100 {
-          // discard the request.
-          conn.Close()
-        } else if kv.unreliable && (rand.Int63() % 1000) < 200 {
-          // process the request but force discard of reply.
-          c1 := conn.(*net.UnixConn)
-          f, _ := c1.File()
-          err := syscall.Shutdown(int(f.Fd()), syscall.SHUT_WR)
-          if err != nil {
-            fmt.Printf("shutdown: %v\n", err)
-          }
-          go rpcs.ServeConn(conn)
-        } else {
-          go rpcs.ServeConn(conn)
-        }
-      } else if err == nil {
-        conn.Close()
-      }
-      if err != nil && kv.dead == false {
-        fmt.Printf("KVPaxos(%v) accept: %v\n", me, err.Error())
-        kv.kill()
-      }
-    }
-  }()
-
-  return kv
+	return kv
 }
-
